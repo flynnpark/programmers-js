53834520a73819cca9db5ef26e99556da9d4348c
Flynn <wphestiraid@gmail.com>
2022-12-12 10:26:19 +0900

    디펜스 게임 문제 해결

--- solutions/level2/defenseGame/solution.js
+++ solutions/level2/defenseGame/solution.js
@@ -1,3 +1,28 @@
 function solution(n, k, enemy) {
-  return -1;
+  let left = 0;
+  let right = enemy.length;
+
+  while (true) {
+    if (right - left === 1) break;
+
+    const mid = Math.floor((left + right) / 2);
+    const sum =
+      mid + 1 - k > 0
+        ? enemy
+            .slice(0, mid + 1)
+            .sort((a, b) => a - b)
+            .slice(0, mid + 1 - k)
+            .reduce((a, b) => a + b, 0)
+        : 0;
+
+    if (n >= sum) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+
+  return right;
 }
+
+export { solution };
---
+++ solutions/level2/defenseGame/solution.test.js
@@ -0,0 +1,26 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      n: 7,
+      k: 3,
+      enemy: [4, 2, 4, 5, 3, 3, 1],
+    },
+    result: 5,
+  },
+  {
+    input: {
+      n: 2,
+      k: 4,
+      enemy: [3, 3, 3, 3],
+    },
+    result: 4,
+  },
+];
+
+test('디펜스 게임', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
