82960d4458e69c337dfa3dac983e6994aeefbab3
Flynn <wphestiraid@gmail.com>
2022-12-15 08:48:37 +0900

    과일장수 문제 해결

--- solutions/level1/fruiterer/solution.js
+++ solutions/level1/fruiterer/solution.js
@@ -1,4 +1,15 @@
 function solution(k, m, score) {
   let result = 0;
+  score.sort((a, b) => b - a);
+
+  for (let i = 0; i < score.length; i += m) {
+    const chunk = score.slice(i, i + m);
+    if (chunk.length === m) {
+      result += Math.min(...chunk) * m;
+    }
+  }
+
   return result;
 }
+
+export { solution };
---
+++ solutions/level1/fruiterer/solution.test.js
@@ -0,0 +1,26 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      k: 3,
+      m: 4,
+      score: [1, 2, 3, 1, 2, 3, 1],
+    },
+    result: 8,
+  },
+  {
+    input: {
+      k: 4,
+      m: 3,
+      score: [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2],
+    },
+    result: 33,
+  },
+];
+
+test('과일 장수', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
