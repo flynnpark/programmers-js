3262f95baedd70fb2b9e5d0a67c6766f338361ce
Flynn <wphestiraid@gmail.com>
2022-12-16 09:58:01 +0900

    실패율 문제 해결

--- solutions/level1/failureRate/solution.js
+++ solutions/level1/failureRate/solution.js
@@ -1,4 +1,33 @@
 function solution(N, stages) {
-  let result = [];
-  return result;
+  const stageMembers = {};
+
+  for (const stage of stages) {
+    if (stageMembers[stage]) {
+      stageMembers[stage] += 1;
+    } else {
+      stageMembers[stage] = 1;
+    }
+  }
+
+  let challengers = stages.length;
+  const failure = {};
+  for (let stage = 1; stage <= N; stage++) {
+    failure[stage] = stageMembers[stage]
+      ? stageMembers[stage] / challengers
+      : 0;
+    if (stageMembers[stage]) {
+      challengers -= stageMembers[stage];
+    }
+  }
+
+  const rankBoard = [];
+  for (const stage of Object.keys(failure)) {
+    rankBoard.push([stage, failure[stage]]);
+  }
+
+  rankBoard.sort((a, b) => b[1] - a[1]);
+
+  return rankBoard.map((item) => Number(item[0]));
 }
+
+export { solution };
---
+++ solutions/level1/failureRate/solution.test.js
@@ -0,0 +1,24 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      N: 5,
+      stages: [2, 1, 2, 6, 2, 4, 3, 3],
+    },
+    result: [3, 4, 2, 1, 5],
+  },
+  {
+    input: {
+      N: 4,
+      stages: [4, 4, 4, 4, 4],
+    },
+    result: [4, 1, 2, 3],
+  },
+];
+
+test('실패율', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
