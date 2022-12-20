519dd3e0e027f7f75353e698f42893231938f27a
Flynn <wphestiraid@gmail.com>
2022-12-20 09:06:49 +0900

    연속 부분 수열 합의 개수 문제 해결

--- solutions/level2/theNumberOfSumsOfConsecutiveSubsqeuences/solution.js
+++ solutions/level2/theNumberOfSumsOfConsecutiveSubsqeuences/solution.js
@@ -1,4 +1,15 @@
 function solution(elements) {
-  let result = 0;
-  return result;
+  const countSet = new Set();
+
+  for (let i = 1; i <= elements.length; i++) {
+    const temp = [...elements, ...elements.slice(0, i)];
+
+    for (let j = 0; j < temp.length; j++) {
+      countSet.add(temp.slice(j, j + i).reduce((a, b) => a + b, 0));
+    }
+  }
+
+  return countSet.size;
 }
+
+export { solution };
---
+++ solutions/level2/theNumberOfSumsOfConsecutiveSubsqeuences/solution.test.js
@@ -0,0 +1,16 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      elements: [7, 9, 1, 1, 4],
+    },
+    result: 18,
+  },
+];
+
+test('연속 부분 수열 합의 개수', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
