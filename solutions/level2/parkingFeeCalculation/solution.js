f7f5a9399fb58c4d1875c9f29f90dda6ca20514f
Flynn <wphestiraid@gmail.com>
2022-12-22 09:46:16 +0900

    주차 요금 계산 문제 해결

--- solutions/level2/parkingFeeCalculation/solution.js
+++ solutions/level2/parkingFeeCalculation/solution.js
@@ -1,4 +1,64 @@
-function solution(fees, records) {
-  let result = [];
-  return result;
+function parseRecord(record) {
+  const [timeString, carNumber, type] = record.split(' ');
+  return {
+    timeString,
+    carNumber,
+    type,
+  };
 }
+
+function parseTimeString(timeString) {
+  const [hours, minutes] = timeString.split(':');
+  return Number(hours) * 60 + Number(minutes);
+}
+
+function upsertTime(totalTimeObject, carNumber, time) {
+  if (totalTimeObject[carNumber]) {
+    totalTimeObject[carNumber] += time;
+  } else {
+    totalTimeObject[carNumber] = time;
+  }
+}
+
+function calculateParkingFee(fees, totalTime) {
+  const [firstTime, firstFee, unitTime, unitFee] = fees;
+
+  if (totalTime <= firstTime) {
+    return firstFee;
+  }
+  return firstFee + Math.ceil((totalTime - firstTime) / unitTime) * unitFee;
+}
+
+function solution(fees, records) {
+  const totalTimeObject = {};
+  const recordObject = {};
+  for (const record of records) {
+    const { timeString, carNumber, type } = parseRecord(record);
+    const time = parseTimeString(timeString);
+
+    if (type === 'IN') {
+      recordObject[carNumber] = time;
+    } else {
+      const totalTime = time - recordObject[carNumber];
+      upsertTime(totalTimeObject, carNumber, totalTime);
+      delete recordObject[carNumber];
+    }
+  }
+
+  for (const carNumber of Object.keys(recordObject)) {
+    const time = parseTimeString('23:59');
+    const totalTime = time - recordObject[carNumber];
+    upsertTime(totalTimeObject, carNumber, totalTime);
+    delete recordObject[carNumber];
+  }
+
+  const totalFees = [];
+  for (const carNumber of Object.keys(totalTimeObject)) {
+    const fee = calculateParkingFee(fees, totalTimeObject[carNumber]);
+    totalFees.push([carNumber, fee]);
+  }
+
+  return totalFees.sort().map((item) => item[1]);
+}
+
+export { solution };
---
+++ solutions/level2/parkingFeeCalculation/solution.test.js
@@ -0,0 +1,47 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      fees: [180, 5000, 10, 600],
+      records: [
+        '05:34 5961 IN',
+        '06:00 0000 IN',
+        '06:34 0000 OUT',
+        '07:59 5961 OUT',
+        '07:59 0148 IN',
+        '18:59 0000 IN',
+        '19:09 0148 OUT',
+        '22:59 5961 IN',
+        '23:00 5961 OUT',
+      ],
+    },
+    result: [14600, 34400, 5000],
+  },
+  {
+    input: {
+      fees: [120, 0, 60, 591],
+      records: [
+        '16:00 3961 IN',
+        '16:00 0202 IN',
+        '18:00 3961 OUT',
+        '18:00 0202 OUT',
+        '23:58 3961 IN',
+      ],
+    },
+    result: [0, 591],
+  },
+  {
+    input: {
+      fees: [1, 461, 1, 10],
+      records: ['00:00 1234 IN'],
+    },
+    result: [14841],
+  },
+];
+
+test('주차 요금 계산', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
