4f5c2fe1e920fd007f2c51e3fab9a735e052811e
Flynn <wphestiraid@gmail.com>
2022-12-25 21:43:16 +0900

    모의고사 문제 해결

--- solutions/level1/mockExam/solution.js
+++ solutions/level1/mockExam/solution.js
@@ -1,4 +1,30 @@
 function solution(answers) {
-  let result = [];
-  return result;
+  const correctCounts = [0, 0, 0];
+  const no1Numbers = [1, 2, 3, 4, 5];
+  const no2Numbers = [2, 1, 2, 3, 2, 4, 2, 5];
+  const no3Numbers = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === no1Numbers[i % no1Numbers.length]) {
+      correctCounts[0]++;
+    }
+    if (answers[i] === no2Numbers[i % no2Numbers.length]) {
+      correctCounts[1]++;
+    }
+    if (answers[i] === no3Numbers[i % no3Numbers.length]) {
+      correctCounts[2]++;
+    }
+  }
+
+  const maxCounts = Math.max(...correctCounts);
+  const results = [];
+
+  correctCounts.forEach((value, index) => {
+    if (value === maxCounts) {
+      results.push(index + 1);
+    }
+  });
+  return results;
 }
+
+export { solution };
---
+++ solutions/level1/mockExam/solution.test.js
@@ -0,0 +1,22 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      answers: [1, 2, 3, 4, 5],
+    },
+    result: [1],
+  },
+  {
+    input: {
+      answers: [1, 3, 2, 4, 2],
+    },
+    result: [1, 2, 3],
+  },
+];
+
+test('모의고사', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
