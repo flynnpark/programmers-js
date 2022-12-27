8624e2fa183074fc0987ca4fb738014b1a962f2c
Flynn <wphestiraid@gmail.com>
2022-12-27 10:03:38 +0900

    소수 만들기 문제 해결

--- solutions/level1/makingPrime/solution.js
+++ solutions/level1/makingPrime/solution.js
@@ -1,4 +1,35 @@
+const MAX_NUMS_LENGTH = 3;
+
+function isPrimeNumber(num) {
+  for (let i = 2; i ** 2 <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function solution(nums) {
   let result = 0;
+  const sets = [];
+
+  function dfs(prevSet, restNums) {
+    if (prevSet.length === MAX_NUMS_LENGTH) {
+      const sum = prevSet.reduce((a, b) => a + b, 0);
+      if (isPrimeNumber(sum)) {
+        result++;
+      }
+      return;
+    }
+
+    for (let i = 0; i < restNums.length; i++) {
+      dfs([...prevSet, restNums[i]], [...restNums.slice(i + 1)]);
+    }
+  }
+
+  dfs([], nums);
+
   return result;
 }
+
+export { solution };
---
+++ solutions/level1/makingPrime/solution.test.js
@@ -0,0 +1,22 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      nums: [1, 2, 3, 4],
+    },
+    result: 1,
+  },
+  {
+    input: {
+      nums: [1, 2, 7, 6, 4],
+    },
+    result: 4,
+  },
+];
+
+test('소수 만들기', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
