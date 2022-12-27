201ba11ed75fc81d36cbe57f8595060645a40312
Flynn <wphestiraid@gmail.com>
2022-12-27 10:20:02 +0900

    행렬의 덧셈 문제 해결

--- solutions/level1/matrixAddition/solution.js
+++ solutions/level1/matrixAddition/solution.js
@@ -1,4 +1,14 @@
 function solution(arr1, arr2) {
-  let result = [[]];
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    const row = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      const sum = arr1[i][j] + arr2[i][j];
+      row.push(sum);
+    }
+    result.push(row);
+  }
   return result;
 }
+
+export { solution };
---
+++ solutions/level1/matrixAddition/solution.test.js
@@ -0,0 +1,33 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      arr1: [
+        [1, 2],
+        [2, 3],
+      ],
+      arr2: [
+        [3, 4],
+        [5, 6],
+      ],
+    },
+    result: [
+      [4, 6],
+      [7, 9],
+    ],
+  },
+  {
+    input: {
+      arr1: [[1], [2]],
+      arr2: [[3], [4]],
+    },
+    result: [[4], [6]],
+  },
+];
+
+test('행렬의 덧셈', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
