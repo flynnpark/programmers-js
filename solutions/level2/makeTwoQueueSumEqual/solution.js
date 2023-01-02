35c47d09e6dcf3b1f8bd56329aa60bd3851d50f0
Flynn <wphestiraid@gmail.com>
2023-01-02 09:58:05 +0900

    두 큐 합 같게 만들기 문제 해결

--- solutions/level2/makeTwoQueueSumEqual/solution.js
+++ solutions/level2/makeTwoQueueSumEqual/solution.js
@@ -1,4 +1,26 @@
 function solution(queue1, queue2) {
   let result = 0;
-  return 0;
+  const max = queue1.length * 3;
+  let [i1, i2] = [0, 0];
+  let diff = queue1.reduce((acc, cur, idx) => acc + cur - queue2[idx], 0) / 2;
+
+  while (result < max) {
+    if (diff > 0) {
+      const value = queue1[i1];
+      i1++;
+      diff -= value;
+      queue2.push(value);
+    } else if (diff < 0) {
+      const value = queue2[i2];
+      i2++;
+      diff += value;
+      queue1.push(value);
+    } else {
+      break;
+    }
+    result++;
+  }
+  return diff !== 0 ? -1 : result;
 }
+
+export { solution };
---
+++ solutions/level2/makeTwoQueueSumEqual/solution.test.js
@@ -0,0 +1,31 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      queue1: [3, 2, 7, 2],
+      queue2: [4, 6, 5, 1],
+    },
+    result: 2,
+  },
+  {
+    input: {
+      queue1: [1, 2, 1, 2],
+      queue2: [1, 10, 1, 2],
+    },
+    result: 7,
+  },
+  {
+    input: {
+      queue1: [1, 1],
+      queue2: [1, 5],
+    },
+    result: 1,
+  },
+];
+
+test('테이블 해시 함수', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
