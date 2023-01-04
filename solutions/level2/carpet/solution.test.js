8a280e2cf5af6e99c50bdcd35d15cf266c7c9f70
Flynn <wphestiraid@gmail.com>
2023-01-05 08:08:58 +0900

    카펫 문제 추가

---
+++ solutions/level2/carpet/solution.test.js
@@ -0,0 +1,31 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      brown: 10,
+      yellow: 2,
+    },
+    result: [4, 3],
+  },
+  {
+    input: {
+      brown: 8,
+      yellow: 1,
+    },
+    result: [3, 3],
+  },
+  {
+    input: {
+      brown: 24,
+      yellow: 24,
+    },
+    result: [8, 6],
+  },
+];
+
+test('카펫', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input))).toBe(result);
+  });
+});
