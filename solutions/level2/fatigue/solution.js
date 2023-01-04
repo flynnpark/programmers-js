2076a233dc823b5fa055c4dc61122355793783ae
Flynn <wphestiraid@gmail.com>
2023-01-04 09:13:28 +0900

    피로도 문제 해결

--- solutions/level2/fatigue/solution.js
+++ solutions/level2/fatigue/solution.js
@@ -1,6 +1,21 @@
 function solution(k, dungeons) {
-  var answer = -1;
-  return answer;
+  let result = 0;
+
+  function dfs(prevK, prevDungeons, prevResult) {
+    for (let i = 0; i < prevDungeons.length; i++) {
+      const [need, consume] = prevDungeons[i];
+      if (!need || need > prevK) {
+        continue;
+      }
+      const nextDungeons = [...prevDungeons].map((item) => [...item]);
+      nextDungeons[i] = [null, null];
+      dfs(prevK - consume, nextDungeons, prevResult + 1);
+    }
+    result = Math.max(prevResult, result);
+  }
+
+  dfs(k, dungeons, result);
+  return result;
 }
 
 export { solution };
---
+++ solutions/level2/fatigue/solution.test.js
@@ -0,0 +1,21 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      k: 80,
+      dungeons: [
+        [80, 20],
+        [50, 40],
+        [30, 10],
+      ],
+    },
+    result: 3,
+  },
+];
+
+test('피로도', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input)) === result);
+  });
+});
