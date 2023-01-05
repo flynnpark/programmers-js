897f222759f99ca5039d2619b4e493ad954dfe17
Flynn <wphestiraid@gmail.com>
2023-01-06 08:26:24 +0900

    기능개발 문제 추가

---
+++ solutions/level2/functionDevelopment/solution.js
@@ -0,0 +1,6 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  return answer;
+}
+
+export { solution };
---
+++ solutions/level2/functionDevelopment/solution.test.js
@@ -0,0 +1,24 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      progresses: [93, 30, 55],
+      speeds: [1, 30, 5],
+    },
+    result: [2, 1],
+  },
+  {
+    input: {
+      progresses: [95, 90, 99, 99, 80, 99],
+      speeds: [1, 1, 1, 1, 1, 1],
+    },
+    result: [1, 3, 2],
+  },
+];
+
+test('기능개발', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input))).toStrictEqual(result);
+  });
+});
