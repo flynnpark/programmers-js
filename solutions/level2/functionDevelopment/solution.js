dc4ace0341da27ee03dba2ae0be13820c90fc3f6
Flynn <wphestiraid@gmail.com>
2023-01-06 08:45:41 +0900

    기능개발 문제 해결

--- solutions/level2/functionDevelopment/solution.js
+++ solutions/level2/functionDevelopment/solution.js
@@ -1,6 +1,27 @@
 function solution(progresses, speeds) {
-  var answer = [];
-  return answer;
+  let result = [];
+  let day = 0;
+
+  while (progresses.length > 0) {
+    for (let i = 0; i < progresses.length; i++) {
+      const currentProgress = progresses[i] + speeds[i];
+      progresses[i] = currentProgress > 100 ? 100 : currentProgress;
+    }
+
+    if (progresses[0] === 100) {
+      let count = 0;
+      while (progresses[0] === 100) {
+        progresses.shift();
+        speeds.shift();
+        count++;
+      }
+      result.push(count);
+    }
+
+    day++;
+  }
+
+  return result;
 }
 
 export { solution };
