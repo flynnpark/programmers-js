d9143dbb5e31ee792dc61ec44ce68390fe20038f
Flynn <wphestiraid@gmail.com>
2023-01-05 09:07:46 +0900

    프린터 문제 해결

--- solutions/level2/printer/solution.js
+++ solutions/level2/printer/solution.js
@@ -1,5 +1,20 @@
 function solution(priorities, location) {
   let result = 0;
+
+  while (priorities.length > 0) {
+    const [current, ...rest] = priorities;
+    if (current !== Math.max(...priorities)) {
+      priorities = [...rest, current];
+      if (location === 0) location = priorities.length - 1;
+      else location--;
+    } else {
+      priorities = rest;
+      result++;
+      if (location === 0) break;
+      else location--;
+    }
+  }
+
   return result;
 }
 
