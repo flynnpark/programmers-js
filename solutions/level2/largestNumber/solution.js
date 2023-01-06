d690b0a82a61a2c98d15f50beaac88f39e7938df
Flynn <wphestiraid@gmail.com>
2023-01-06 09:33:24 +0900

    가장 큰 수 문제 해결

--- solutions/level2/largestNumber/solution.js
+++ solutions/level2/largestNumber/solution.js
@@ -1,6 +1,11 @@
 function solution(numbers) {
-  var answer = '';
-  return answer;
+  const sortedNumbers = numbers
+    .map((number) => `${number}`)
+    .sort((a, b) => b + a - (a + b));
+
+  return sortedNumbers.every((str) => str === '0')
+    ? '0'
+    : sortedNumbers.join('');
 }
 
 export { solution };
