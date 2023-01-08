29eb8fa7ad08d43659bc29df779866b449200897
Flynn <wphestiraid@gmail.com>
2023-01-08 16:45:15 +0900

    소수 찾기 문제 해결

--- solutions/level2/findThePrimeNumber/solution.js
+++ solutions/level2/findThePrimeNumber/solution.js
@@ -1,6 +1,39 @@
+function isPrimeNumber(num) {
+  if ([0, 1].includes(num)) {
+    return false;
+  }
+
+  for (let i = 2; i ** 2 <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function solution(numbers) {
-  var answer = 0;
-  return answer;
+  function dfs(prevNums, restNums) {
+    if (!restNums.length) {
+      return;
+    }
+
+    for (let i = 0; i < restNums.length; i++) {
+      const newPrevNums = [...prevNums];
+      newPrevNums.push(restNums[i]);
+      candidates.add(Number(newPrevNums.join('')));
+      dfs(newPrevNums, [...restNums.slice(0, i), ...restNums.slice(i + 1)]);
+    }
+  }
+
+  let result = 0;
+  const candidates = new Set();
+  dfs([], numbers.split(''));
+
+  for (let item of candidates) {
+    if (isPrimeNumber(item)) result++;
+  }
+
+  return result;
 }
 
 export { solution };
