8443c672d04bdc81263e908f8c7c8cbf1bc6e130
Flynn <wphestiraid@gmail.com>
2023-01-16 08:31:20 +0900

    삼각 달팽이 문제 추가

---
+++ solutions/level2/triangularSnail/solution.test.js
@@ -0,0 +1,30 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      n: 4,
+    },
+    result: [1, 2, 9, 3, 10, 8, 4, 5, 6, 7],
+  },
+  {
+    input: {
+      n: 5,
+    },
+    result: [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9],
+  },
+  {
+    input: {
+      n: 6,
+    },
+    result: [
+      1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11,
+    ],
+  },
+];
+
+test('삼각 달팽이', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input))).toBe(result);
+  });
+});
