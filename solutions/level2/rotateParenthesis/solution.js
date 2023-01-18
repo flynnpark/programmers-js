14692b320218dbf624ccc00d55f24275f38c466f
Flynn <wphestiraid@gmail.com>
2023-01-19 08:32:32 +0900

    괄호 회전하기 문제 추가

---
+++ solutions/level2/rotateParenthesis/solution.js
@@ -0,0 +1,6 @@
+function solution(s) {
+  var answer = -1;
+  return answer;
+}
+
+export { solution };
---
+++ solutions/level2/rotateParenthesis/solution.test.js
@@ -0,0 +1,34 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      s: '[](){}',
+    },
+    result: 3,
+  },
+  {
+    input: {
+      s: '}]()[{',
+    },
+    result: 2,
+  },
+  {
+    input: {
+      s: '[)(]',
+    },
+    result: 0,
+  },
+  {
+    input: {
+      s: '}}}',
+    },
+    result: 0,
+  },
+];
+
+test('괄호 회전하기', () => {
+  cases.forEach(({ input, result }) => {
+    expect(solution(...Object.values(input))).toBe(result);
+  });
+});
