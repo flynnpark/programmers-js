62bc307bdb4fc13792884faf584c85e43b4c09a1
Flynn <wphestiraid@gmail.com>
2023-01-24 14:18:44 +0900

    다리를 지나는 트럭 문제 해결

--- solutions/level2/truckPassingTheBridge/solution.js
+++ solutions/level2/truckPassingTheBridge/solution.js
@@ -1,6 +1,21 @@
 function solution(bridge_length, weight, truck_weights) {
-  var answer = 0;
-  return answer;
+  let result = 0;
+  const bridge = Array(bridge_length).fill(0);
+  let weightSum = 0;
+
+  while (truck_weights.length > 0 || weightSum > 0) {
+    result++;
+    weightSum -= bridge.shift();
+
+    if (truck_weights.length > 0 && weightSum + truck_weights[0] <= weight) {
+      weightSum += truck_weights[0];
+      bridge.push(truck_weights.shift());
+    } else {
+      bridge.push(0);
+    }
+  }
+
+  return result;
 }
 
 export { solution };
