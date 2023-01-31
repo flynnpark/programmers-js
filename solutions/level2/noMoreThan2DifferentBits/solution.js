51e72c13ab5a3a5a1fb41e69d4799b20d76ae04a
Flynn <wphestiraid@gmail.com>
2023-01-31 09:11:36 +0900

    2개 이하로 다른 비트 문제 해결

--- solutions/level2/noMoreThan2DifferentBits/solution.js
+++ solutions/level2/noMoreThan2DifferentBits/solution.js
@@ -1,6 +1,20 @@
 function solution(numbers) {
-  var answer = [];
-  return answer;
+  const result = [];
+
+  for (let number of numbers) {
+    if (number % 2 === 0) {
+      result.push(number + 1);
+    } else {
+      let diff = 2;
+      while (true) {
+        if ((number + 1) % (diff * 2) === 0) diff = diff * 2;
+        else break;
+      }
+      result.push(number + diff / 2);
+    }
+  }
+
+  return result;
 }
 
 export { solution };
