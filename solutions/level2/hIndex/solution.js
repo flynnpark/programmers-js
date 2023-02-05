9214682fa67165e9aae48840fb832d380d8b1c23
Flynn <wphestiraid@gmail.com>
2023-02-06 08:31:39 +0900

    H-Index 문제 해결

--- solutions/level2/hIndex/solution.js
+++ solutions/level2/hIndex/solution.js
@@ -1,6 +1,11 @@
 function solution(citations) {
-  var answer = 0;
-  return answer;
+  citations = citations.sort((a, b) => a - b);
+  let result = citations.length;
+  for (; result > 0; result--) {
+    if (citations.filter((citation) => citation >= result).length >= result)
+      break;
+  }
+  return result;
 }
 
 export { solution };
