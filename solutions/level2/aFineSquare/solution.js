96fe6f5de3047e8fb000f9e6f1366010c4eb543c
Flynn <wphestiraid@gmail.com>
2023-02-07 08:51:47 +0900

    멀쩡한 사각형 문제 해결

--- solutions/level2/aFineSquare/solution.js
+++ solutions/level2/aFineSquare/solution.js
@@ -1,6 +1,10 @@
+function gcd(num1, num2) {
+  if (num2 === 0) return num1;
+  return gcd(num2, num1 % num2);
+}
+
 function solution(w, h) {
-  var answer = 1;
-  return answer;
+  return w * h - (w + h - gcd(w, h));
 }
 
 export { solution };
