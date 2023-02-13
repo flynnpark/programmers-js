d141097f9a68f68582181da60035bcbc8b7122b2
Flynn <wphestiraid@gmail.com>
2023-02-13 09:00:06 +0900

    숫자 변환하기 문제 해결

--- solutions/level2/convertingNumbers/solution.js
+++ solutions/level2/convertingNumbers/solution.js
@@ -1,6 +1,25 @@
 function solution(x, y, n) {
-  var answer = 0;
-  return answer;
+  if (x === y) return 0;
+
+  const dp = {};
+  dp[x] = 0;
+
+  let data = [x];
+
+  while (data.length) {
+    const newData = [];
+    for (const item of data) {
+      for (const next of [item + n, item * 2, item * 3]) {
+        if (next > y || dp[next]) continue;
+        if (next === y) return dp[item] + 1;
+        dp[next] = dp[item] + 1;
+        newData.push(next);
+      }
+    }
+    data = newData;
+  }
+
+  return -1;
 }
 
 export { solution };
