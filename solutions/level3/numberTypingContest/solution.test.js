b12413c4e9fe20b66263cd0a3124dac146116ad9
Flynn <wphestiraid@gmail.com>
2023-09-14 08:18:13 +0900

    problem: 숫자 타자 대회

---
+++ solutions/level3/numberTypingContest/solution.test.js
@@ -0,0 +1,22 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      numbers: '1756',
+    },
+    result: 10,
+  },
+  {
+    input: {
+      numbers: '5123',
+    },
+    result: 8,
+  },
+];
+
+describe('숫자 타자 대회', () => {
+  test.each(cases)('%o', ({ input, result }) => {
+    expect(solution(...Object.values(input))).toStrictEqual(result);
+  });
+});
