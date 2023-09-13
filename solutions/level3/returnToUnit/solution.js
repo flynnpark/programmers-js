25cc844306c8cd5e03a8aa89d00407fadef3bcfe
Flynn <wphestiraid@gmail.com>
2023-09-13 09:46:43 +0900

    solution: 부대 복귀

--- solutions/level3/returnToUnit/solution.js
+++ solutions/level3/returnToUnit/solution.js
@@ -1,6 +1,27 @@
 function solution(n, roads, sources, destination) {
-  var answer = [];
-  return answer;
+  const visited = new Array(n + 1).fill(Infinity);
+  const connected = new Array(n + 1).fill(0).map(() => []);
+
+  roads.forEach(([from, to]) => {
+    connected[from].push(to);
+    connected[to].push(from);
+  });
+
+  const queue = [destination];
+  visited[destination] = 0;
+  while (queue.length) {
+    const current = queue.shift();
+    for (const next of connected[current]) {
+      if (visited[current] + 1 < visited[next]) {
+        visited[next] = visited[current] + 1;
+        queue.push(next);
+      }
+    }
+  }
+
+  return sources.map((source) =>
+    visited[source] !== Infinity ? visited[source] : -1,
+  );
 }
 
 export { solution };
