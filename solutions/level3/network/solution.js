d1a6c1a9dda1b9d6601cd67581fe1d6c9b6c1355
Flynn <wphestiraid@gmail.com>
2023-09-19 09:29:08 +0900

    solution: 네트워크

--- solutions/level3/network/solution.js
+++ solutions/level3/network/solution.js
@@ -1,6 +1,24 @@
 function solution(n, computers) {
-  var answer = 0;
-  return answer;
+  let result = 0;
+  let visited = Array(n).fill(false);
+
+  const dfs = (i) => {
+    visited[i] = true;
+    for (let j = 0; j < n; j++) {
+      if (computers[i][j] === 1 && !visited[j]) {
+        dfs(j);
+      }
+    }
+  };
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      result++;
+    }
+  }
+
+  return result;
 }
 
 export { solution };
