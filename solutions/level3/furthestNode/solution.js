b87e96b6d8b44059401fdc51cb5ca285a84009eb
Flynn <wphestiraid@gmail.com>
2023-09-22 08:55:29 +0900

    solution: 가장 먼 노드

--- solutions/level3/furthestNode/solution.js
+++ solutions/level3/furthestNode/solution.js
@@ -1,6 +1,31 @@
 function solution(n, edge) {
-  var answer = 0;
-  return answer;
+  const graph = Array.from({ length: n + 1 }, () => []);
+
+  edge.forEach((node) => {
+    const [from, to] = node;
+    graph[from].push(to);
+    graph[to].push(from);
+  });
+
+  const visited = new Array(n + 1).fill(0);
+  const queue = [1];
+  visited[1] = 1;
+
+  while (queue.length) {
+    const current = queue.shift();
+    const nextNodes = graph[current];
+
+    for (const next of nextNodes) {
+      if (!visited[next]) {
+        queue.push(next);
+        visited[next] = visited[current] + 1;
+      }
+    }
+  }
+
+  const maxDistance = Math.max(...visited);
+
+  return visited.filter((item) => item === maxDistance).length;
 }
 
 export { solution };
