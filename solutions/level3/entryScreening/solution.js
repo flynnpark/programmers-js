47a142c317e183a31f25acebfc3dcae3049901eb
Flynn <wphestiraid@gmail.com>
2023-09-29 19:08:40 +0900

    solution: 입국심사

--- solutions/level3/entryScreening/solution.js
+++ solutions/level3/entryScreening/solution.js
@@ -1,6 +1,13 @@
 function solution(n, times) {
-  var answer = 0;
-  return answer;
+  let min = 1;
+  let max = Math.max(...times) * n;
+  while (min < max) {
+    const mid = Math.floor((min + max) / 2);
+    const people = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
+    if (people >= n) max = mid;
+    else min = mid + 1;
+  }
+  return min;
 }
 
 export { solution };
