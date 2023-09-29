0d858b854390bf1947a6de8d5a53f4634403b1cc
Flynn <wphestiraid@gmail.com>
2023-09-29 09:25:15 +0900

    solution: 섬 연결하기

--- solutions/level3/connectingTheIslands/solution.js
+++ solutions/level3/connectingTheIslands/solution.js
@@ -1,6 +1,37 @@
 function solution(n, costs) {
-  var answer = 0;
-  return answer;
+  const isVisited = new Array(n).fill(false);
+  const isSelected = new Array(costs.length).fill(false);
+  let edges = 0; // 간선 갯수
+  let result = 0;
+
+  costs.sort((a, b) => a[2] - b[2]);
+
+  isVisited[costs[0][0]] = true;
+  isVisited[costs[0][1]] = true;
+  isSelected[0] = true;
+  edges += 1;
+  result += costs[0][2];
+
+  while (edges < n - 1) {
+    for (let i = 1; i < costs.length; i++) {
+      const [from, to, cost] = costs[i];
+
+      if (
+        !isSelected[i] &&
+        ((isVisited[from] && !isVisited[to]) ||
+          (!isVisited[from] && isVisited[to]))
+      ) {
+        isVisited[from] = true;
+        isVisited[to] = true;
+        isSelected[i] = true;
+        result += cost;
+        edges += 1;
+        break;
+      }
+    }
+  }
+
+  return result;
 }
 
 export { solution };
