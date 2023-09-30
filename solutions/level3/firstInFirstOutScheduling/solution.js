3591b51cff385ef0177bb4b93114ac05f0e5b0b7
Flynn <wphestiraid@gmail.com>
2023-09-30 11:30:08 +0900

    solution: 선입 선출 스케줄링

--- solutions/level3/firstInFirstOutScheduling/solution.js
+++ solutions/level3/firstInFirstOutScheduling/solution.js
@@ -1,5 +1,40 @@
 function solution(n, cores) {
-  var answer = 0;
-  return answer;
+  if (n < cores.length) {
+    return n;
+  }
+
+  n = n - cores.length;
+  let left = 1;
+  let right = (Math.max(...cores) * n) / cores.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    let capacity = 0;
+
+    for (const core of cores) {
+      capacity += Math.floor(mid / core);
+    }
+
+    if (capacity >= n) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  for (const core of cores) {
+    n -= Math.floor((left - 1) / core);
+  }
+
+  for (let i = 0; i < cores.length; i++) {
+    if (left % cores[i] === 0) {
+      n--;
+
+      if (n === 0) {
+        return i + 1;
+      }
+    }
+  }
 }
+
 export { solution };
