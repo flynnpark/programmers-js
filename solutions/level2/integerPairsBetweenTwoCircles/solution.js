77102e481e9add7de3c2cc9134ec795ac2821363
Flynn <wphestiraid@gmail.com>
2023-10-01 23:00:47 +0900

    solution: 두 원 사이의 정수 쌍

--- solutions/level2/integerPairsBetweenTwoCircles/solution.js
+++ solutions/level2/integerPairsBetweenTwoCircles/solution.js
@@ -1,6 +1,13 @@
 function solution(r1, r2) {
-  var answer = 0;
-  return answer;
+  let result = 0;
+
+  for (let x = 1; x <= r2; x++) {
+    const maxY = Math.floor(Math.sqrt(r2 ** 2 - x ** 2));
+    const minY = r1 <= x ? 0 : Math.ceil(Math.sqrt(r1 ** 2 - x ** 2));
+
+    result += maxY - minY + 1;
+  }
+  return result * 4;
 }
 
 export { solution };
