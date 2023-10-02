e84cbbf240f50892427683d355aae0c37cab46af
Flynn <wphestiraid@gmail.com>
2023-10-02 21:15:06 +0900

    solution: 연속된 부분 수열의 합

--- solutions/level2/sumOfConsecutiveSubsequences/solution.js
+++ solutions/level2/sumOfConsecutiveSubsequences/solution.js
@@ -1,6 +1,23 @@
 function solution(sequence, k) {
-  var answer = [];
-  return answer;
+  let result = [0, 0];
+  const length = sequence.length;
+  let right = 0;
+  let sum = 0;
+  let prevDistance = k;
+
+  for (let left = 0; left < length; left++) {
+    while (right < length && sum < k) {
+      sum += sequence[right++];
+    }
+    const currentRight = right - 1;
+    if (sum === k && currentRight - left < prevDistance) {
+      result = [left, currentRight];
+      prevDistance = currentRight - left;
+    }
+    sum -= sequence[left];
+  }
+
+  return result;
 }
 
 export { solution };
