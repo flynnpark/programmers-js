aa6193137c680cac09cb102eb7ddfd85f255bec4
Flynn <wphestiraid@gmail.com>
2023-10-05 08:44:47 +0900

    solution: 뒤에 있는 큰 수 찾기

--- solutions/level2/findTheNextBigNumber/solution.js
+++ solutions/level2/findTheNextBigNumber/solution.js
@@ -1,6 +1,15 @@
 function solution(numbers) {
-  var answer = [];
-  return answer;
+  const result = new Array(numbers.length).fill(-1);
+  const stack = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length && numbers[i] > numbers[stack[stack.length - 1]]) {
+      result[stack.pop()] = numbers[i];
+    }
+    stack.push(i);
+  }
+
+  return result;
 }
 
 export { solution };
