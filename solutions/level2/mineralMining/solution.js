316ccbaa5b5da9375717b111317f80bceac7fbd3
Flynn <wphestiraid@gmail.com>
2023-10-10 10:08:49 +0900

    solution: 광물 캐기

--- solutions/level2/mineralMining/solution.js
+++ solutions/level2/mineralMining/solution.js
@@ -1,6 +1,39 @@
 function solution(picks, minerals) {
-  var answer = 0;
-  return answer;
+  const partLength = 5;
+  const totalPicks = picks.reduce((a, b) => a + b, 0);
+
+  minerals.splice(totalPicks * partLength);
+
+  const valueMap = {
+    diamond: 25,
+    iron: 5,
+    stone: 1,
+  };
+
+  const candidates = [];
+  for (let i = 0; i < minerals.length; i += partLength) {
+    const part = minerals.slice(i, i + partLength);
+    const value = part.reduce((acc, mineral) => acc + valueMap[mineral], 0);
+    candidates.push([part, value]);
+  }
+
+  candidates.sort((a, b) => b[1] - a[1]);
+
+  let result = 0;
+  candidates.forEach(([part]) => {
+    const currentToolIndex = picks.findIndex((pick) => pick !== 0);
+    if (currentToolIndex !== -1) {
+      picks[currentToolIndex]--;
+    }
+
+    for (const mineral of part) {
+      const mineralIndex = Object.keys(valueMap).indexOf(mineral);
+      const gap = currentToolIndex - mineralIndex;
+      result += gap <= 0 ? 1 : 5 ** gap;
+    }
+  });
+
+  return result;
 }
 
 export { solution };
