38d7647cb49313242f02cdc8c4cb44fad986ab8c
Flynn <wphestiraid@gmail.com>
2023-10-12 09:14:21 +0900

    solution: 호텔 대실

--- solutions/level2/hotelRooms/solution.js
+++ solutions/level2/hotelRooms/solution.js
@@ -1,6 +1,30 @@
 function solution(book_time) {
-  var answer = 0;
-  return answer;
+  const bookTimes = book_time
+    .map(([start, end]) => [timeToMinute(start), timeToMinute(end)])
+    .sort((a, b) => a[0] - b[0]);
+
+  const rooms = [bookTimes.shift()];
+
+  bookTimes.forEach((currentTime) => {
+    const [currentStart, currentEnd] = currentTime;
+
+    const isFull = rooms.every(([start, end]) => end + 10 > currentStart);
+    if (isFull) {
+      rooms.push(currentTime);
+    } else {
+      const roomNumber = rooms.findIndex(
+        ([start, end]) => end + 10 <= currentStart,
+      );
+      rooms[roomNumber] = currentTime;
+    }
+  });
+
+  return rooms.length;
+}
+
+function timeToMinute(time) {
+  const [hour, minute] = time.split(':');
+  return Number(hour) * 60 + Number(minute);
 }
 
 export { solution };
