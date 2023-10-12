59cbee3fec78738646650b21cb60e662db861450
Flynn <wphestiraid@gmail.com>
2023-10-13 08:50:42 +0900

    solution: 시소 짝꿍

--- solutions/level2/seesawPairing/solution.js
+++ solutions/level2/seesawPairing/solution.js
@@ -1,6 +1,19 @@
 function solution(weights) {
-  var answer = 0;
-  return answer;
+  weights.sort((a, b) => a - b);
+
+  const weightsMap = {};
+  weights.forEach((weight) => {
+    weightsMap[weight] = (weightsMap[weight] || 0) + 1;
+  });
+
+  return weights.reduce((acc, weight) => {
+    weightsMap[weight]--;
+    if (weightsMap[weight]) acc += weightsMap[weight];
+    if (weightsMap[weight * 2]) acc += weightsMap[weight * 2];
+    if (weightsMap[(weight / 2) * 3]) acc += weightsMap[(weight / 2) * 3];
+    if (weightsMap[(weight / 3) * 4]) acc += weightsMap[(weight / 3) * 4];
+    return acc;
+  }, 0);
 }
 
 export { solution };
