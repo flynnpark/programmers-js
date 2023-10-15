d8d729dcbf0b6c9cceb9458be778c325628643f8
Flynn <wphestiraid@gmail.com>
2023-10-15 14:20:41 +0900

    problem: 무인도 여행

---
+++ solutions/level2/uninhabitedIslandTravel/solution.js
@@ -0,0 +1,6 @@
+function solution(maps) {
+  var answer = [];
+  return answer;
+}
+
+export { solution };
---
+++ solutions/level2/uninhabitedIslandTravel/solution.test.js
@@ -0,0 +1,22 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      maps: ['X591X', 'X1X5X', 'X231X', '1XXX1'],
+    },
+    result: [1, 1, 27],
+  },
+  {
+    input: {
+      maps: ['XXX', 'XXX', 'XXX'],
+    },
+    result: [-1],
+  },
+];
+
+describe('무인도 여행', () => {
+  test.each(cases)('%o', ({ input, result }) => {
+    expect(solution(...Object.values(input))).toBe(result);
+  });
+});
