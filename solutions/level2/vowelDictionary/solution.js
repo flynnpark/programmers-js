392b21981e40e3cbdd7ed1b9b8cd77b06bdc3d78
Flynn <wphestiraid@gmail.com>
2023-10-17 08:52:07 +0900

    solution: 모음 사전

--- solutions/level2/vowelDictionary/solution.js
+++ solutions/level2/vowelDictionary/solution.js
@@ -1,6 +1,20 @@
+const vowels = [...'AEIOU'];
+
 function solution(word) {
-  var answer = 0;
-  return answer;
+  const words = [];
+  for (let i = 1; i <= 5; i++) combination(words, '', i);
+  return words.sort().indexOf(word) + 1;
+}
+
+function combination(results, word, restLength) {
+  if (!restLength) {
+    results.push(word);
+    return;
+  }
+
+  vowels.forEach((vowel) =>
+    combination(results, `${word}${vowel}`, restLength - 1),
+  );
 }
 
 export { solution };
