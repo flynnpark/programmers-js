9fa02aab8e1f4409838bed55aaa4a8bcacc471c4
Flynn <wphestiraid@gmail.com>
2023-10-20 08:57:06 +0900

    solution: 요격 시스템

--- solutions/level2/interceptionSystem/solution.js
+++ solutions/level2/interceptionSystem/solution.js
@@ -1,6 +1,19 @@
 function solution(targets) {
-  var answer = 0;
-  return answer;
+  let result = 0;
+  let bound = 0;
+
+  targets
+    .sort((a, b) => a[0] - b[0])
+    .forEach(([start, end]) => {
+      if (bound > start) {
+        bound = Math.min(bound, end);
+      } else {
+        bound = end;
+        result++;
+      }
+    });
+
+  return result;
 }
 
 export { solution };
