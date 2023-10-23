372f211103f8792fe53954d348dfc3bb8feceed5
Flynn <wphestiraid@gmail.com>
2023-10-23 09:11:11 +0900

    solution: 전력망을 둘로 나누기

--- solutions/level2/splittingThePowerGridInTwo/solution.js
+++ solutions/level2/splittingThePowerGridInTwo/solution.js
@@ -1,6 +1,27 @@
 function solution(n, wires) {
-  var answer = -1;
-  return answer;
+  return wires.reduce((prevResult, [currentFrom, currentTo]) => {
+    const connection = {};
+    for (let i = 1; i <= n; i++) connection[i] = [];
+
+    wires.forEach(([from, to]) => {
+      if (!(currentFrom === from && currentTo === to)) {
+        connection[from].push(to);
+        connection[to].push(from);
+      }
+    });
+
+    const queue = [1];
+    const visited = [];
+    while (queue.length) {
+      const current = queue.shift();
+      if (!visited.includes(current)) {
+        queue.push(...connection[current]);
+        visited.push(current);
+      }
+    }
+
+    return Math.min(prevResult, Math.abs(n - 2 * visited.length));
+  }, n);
 }
 
 export { solution };
