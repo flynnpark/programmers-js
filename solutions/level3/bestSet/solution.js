ca0b320c3efcee0a98e15e44cf8daca8700a3aef
Flynn <wphestiraid@gmail.com>
2023-11-02 08:46:53 +0900

    solution: 최고의 집합

--- solutions/level3/bestSet/solution.js
+++ solutions/level3/bestSet/solution.js
@@ -1,6 +1,14 @@
 function solution(n, s) {
-  var answer = [];
-  return answer;
+  if (n > s) return [-1];
+
+  const result = [];
+  for (let i = n; i > 0; i--) {
+    const value = Math.floor(s / i);
+    result.push(value);
+    s -= value;
+  }
+
+  return result;
 }
 
 export { solution };
