93f223cd8082d4895ab4db02c4a00d4b0e0f4e0e
Flynn <wphestiraid@gmail.com>
2023-11-07 08:57:39 +0900

    solution: 감시카메라

--- solutions/level3/surveillanceCamera/solution.js
+++ solutions/level3/surveillanceCamera/solution.js
@@ -1,6 +1,21 @@
 function solution(routes) {
-  var answer = 0;
-  return answer;
+  routes = routes.sort((a, b) => a[1] - b[1]);
+  return routes.reduce(
+    ({ position, result }, [start, end]) => {
+      if (position < start) {
+        position = end;
+        result += 1;
+      }
+      return {
+        position,
+        result,
+      };
+    },
+    {
+      position: -Infinity,
+      result: 0,
+    },
+  ).result;
 }
 
 export { solution };
