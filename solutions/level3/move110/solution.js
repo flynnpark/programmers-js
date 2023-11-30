feadfe891a9b914d46c5260ae1479e9fa438fa1c
Flynn <wphestiraid@gmail.com>
2023-12-01 08:50:15 +0900

    solution: 110 옮기기

--- solutions/level3/move110/solution.js
+++ solutions/level3/move110/solution.js
@@ -1,6 +1,31 @@
 function solution(s) {
-  var answer = [];
-  return answer;
+  return s.map(updateText);
+}
+
+function updateText(text) {
+  const MATCH_TEXT = '110';
+  const stack = [];
+  let matchCount = 0;
+
+  for (const right of text) {
+    if (stack.length > 1 && right === '0') {
+      const middle = stack.pop();
+      const left = stack.pop();
+      `${left}${middle}${right}` === MATCH_TEXT
+        ? matchCount++
+        : stack.push(left, middle, right);
+    } else {
+      stack.push(right);
+    }
+  }
+
+  const baseText = stack.join('');
+  const bestPosition = baseText.lastIndexOf('0') + 1;
+  return bestPosition === -1
+    ? `${MATCH_TEXT.repeat(matchCount)}${baseText}`
+    : `${baseText.slice(0, bestPosition)}${MATCH_TEXT.repeat(
+        matchCount,
+      )}${baseText.slice(bestPosition)}`;
 }
 
 export { solution };
