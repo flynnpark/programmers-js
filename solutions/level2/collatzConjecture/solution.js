f551a0dc8c8d373350f283f6c64b0334afb88dd5
Flynn Park <wphestiraid@gmail.com>
2023-12-11 20:03:27 +0900

    problem: 우박수열 정적분

---
+++ solutions/level2/collatzConjecture/solution.js
@@ -0,0 +1,6 @@
+function solution(k, ranges) {
+  var answer = [];
+  return answer;
+}
+
+export { solution };
---
+++ solutions/level2/collatzConjecture/solution.test.js
@@ -0,0 +1,33 @@
+import { solution } from './solution';
+
+const cases = [
+  {
+    input: {
+      k: 5,
+      ranges: [
+        [0, 0],
+        [0, -1],
+        [2, -3],
+        [3, -3],
+      ],
+    },
+    result: [33.0, 31.5, 0.0, -1.0],
+  },
+  {
+    input: {
+      k: 3,
+      ranges: [
+        [0, 0],
+        [1, -2],
+        [3, -3],
+      ],
+    },
+    result: [47.0, 36.0, 12.0],
+  },
+];
+
+describe('우박수열 정적분', () => {
+  test.each(cases)('%o', ({ input, result }) => {
+    expect(solution(...Object.values(input))).toBe(result);
+  });
+});
